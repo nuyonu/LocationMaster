2f170950c0c222a5c13bcb3afe184f83fc08ca81
aioanedragos <aioane.costin@yahoo.com>
2019-12-16 21:33:19 +0200

    .

---
+++ Maps/Maps/wwwroot/ts/BingTsInterop.ts
@@ -0,0 +1,19 @@
+﻿/// <reference path="types/MicrosoftMaps/Microsoft.Maps.All.d.ts" />
+
+let bingMap: BingMap;
+
+class BingMap {
+    map: Microsoft.Maps.Map;
+
+    constructor() {
+        this.map = new Microsoft.Maps.Map('#myMap', {            
+            center: new Microsoft.Maps.Location(47.6740, 122.1215),   
+            mapTypeId: Microsoft.Maps.MapTypeId.road,
+            zoom: 9
+        });
+    }
+}
+
+function loadMap() : void {
+    bingMap = new BingMap();    
+}
