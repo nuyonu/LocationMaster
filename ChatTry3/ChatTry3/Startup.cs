97c0b8eda851e42ab9c7685770b0100eac5a64e4
aioanedragos <aioane.costin@yahoo.com>
2020-01-03 12:16:17 +0200

    Chat functionality done. Design remain to be done

---
+++ ChatTry3/ChatTry3/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ChatTry3.Startup))]
+
+namespace ChatTry3
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            app.MapSignalR();
+        }
+    }
+}
